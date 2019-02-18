3cde587dc2d606efbeace1adcb5e6e9a2daff759
KitKat <katherine_nguyen7@yahoo.com>
2019-02-17 23:50:56 -0800

    Updates

---
+++ precache-manifest.c24a59ab4c25a2fafa469d755afe7377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Katherine890/ClickMemoryGame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ace1eefcf036902e0a4a",
+    "url": "/Katherine890/ClickMemoryGame/static/js/runtime~main.ace1eefc.js"
+  },
+  {
+    "revision": "b1492f5ca2eff24f353f",
+    "url": "/Katherine890/ClickMemoryGame/static/js/main.b1492f5c.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/Katherine890/ClickMemoryGame/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "b1492f5ca2eff24f353f",
+    "url": "/Katherine890/ClickMemoryGame/static/css/main.56e27332.chunk.css"
+  },
+  {
+    "revision": "21faf9a88f06e44b4b1489578dd43021",
+    "url": "/Katherine890/ClickMemoryGame/index.html"
+  }
+];
