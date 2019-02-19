54b92c525ec5fbae8bb9cadd0fc60a4c0b0042ee
KitKat <katherine_nguyen7@yahoo.com>
2019-02-18 16:11:45 -0800

    updating gh pages

---
+++ build/precache-manifest.14c48cac76ee996097b2f3502fc661a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b977a1daa0e4585f5a393f90fca22b11",
+    "url": "/ClickMemoryGame/static/media/wallpaper.b977a1da.jpg"
+  },
+  {
+    "revision": "dd2d46e279e6b2bde784",
+    "url": "/ClickMemoryGame/static/js/runtime~main.dd2d46e2.js"
+  },
+  {
+    "revision": "b2e2e691729f5ad37734",
+    "url": "/ClickMemoryGame/static/js/main.b2e2e691.chunk.js"
+  },
+  {
+    "revision": "3725fef7b6f9c133ab2a",
+    "url": "/ClickMemoryGame/static/js/2.3725fef7.chunk.js"
+  },
+  {
+    "revision": "b2e2e691729f5ad37734",
+    "url": "/ClickMemoryGame/static/css/main.54c0aea1.chunk.css"
+  },
+  {
+    "revision": "d0a3195d7dee8dc71cb0c43715bf1696",
+    "url": "/ClickMemoryGame/index.html"
+  }
+];
