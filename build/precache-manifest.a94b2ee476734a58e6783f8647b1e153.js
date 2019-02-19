f8ceb566f9d41f978437b76d1bb9528426c3cabe
KitKat <katherine_nguyen7@yahoo.com>
2019-02-18 23:03:38 -0800

    deploying

---
+++ build/precache-manifest.a94b2ee476734a58e6783f8647b1e153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b977a1daa0e4585f5a393f90fca22b11",
+    "url": "/ClickMemoryGame/static/media/wallpaper.b977a1da.jpg"
+  },
+  {
+    "revision": "dd2d46e279e6b2bde784",
+    "url": "/ClickMemoryGame/static/js/runtime~main.dd2d46e2.js"
+  },
+  {
+    "revision": "1641725ff65eaf349606",
+    "url": "/ClickMemoryGame/static/js/main.1641725f.chunk.js"
+  },
+  {
+    "revision": "3725fef7b6f9c133ab2a",
+    "url": "/ClickMemoryGame/static/js/2.3725fef7.chunk.js"
+  },
+  {
+    "revision": "1641725ff65eaf349606",
+    "url": "/ClickMemoryGame/static/css/main.54c0aea1.chunk.css"
+  },
+  {
+    "revision": "91723834de01667dc5a00030cf29c2e1",
+    "url": "/ClickMemoryGame/index.html"
+  }
+];
